af383a0bbc13afe3f7dbebdc69a013e6abdb867b
Eugene <BakulinEU@gmail.com>
2025-01-31 21:40:32 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -21,10 +21,10 @@ balance INTEGER NOT NULL
 #for i in range(1, 11, 3):
 #    cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
 
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", ("60",))
+cursor.execute("SELECT * FROM Users WHERE age != ?", ("60",))
 users = cursor.fetchall()
 for user in users:
-    print(user)
-
+    print(f'Имя:{user[1]}|Почта:{user[2]}|Возраст:{user[3]}|Баланс:{user[4]}')
+    
 connection.commit()
 connection.close()
